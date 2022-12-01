ae2b46dcca38b6c8557de4baf7f4227aa3aa13de
Soner Sayakci <s.sayakci@shopware.com>
2022-12-01 14:17:06 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Shopware Symfony Flex with Devenv
+
+Devenv is a Fast, Declarative, Reproducible, and Composable Developer Environments based on the Nix package manager. 
+
+See [https://devenv.sh/](https://devenv.sh/) for more information.
+
+## Usage
+
+Supported operating systems: Linux, macOS, Windows (WSL2)
+
+- Install [devenv](https://devenv.sh/getting-started/)
+- Run `devenv up` in the project root to start all services
+- Run `devenv shell` to open a shell with all services available
+- Run `bin/console system:install --basic-setup` and open the ready shop at `http://localhost:8000`
+
---
+++ devenv.nix
@@ -0,0 +1,63 @@
+{ pkgs, config, ... }:
+
+let
+  phpPackage = pkgs.php.buildEnv {
+    extensions = { all, enabled }: with all; enabled ++ [ redis ];
+    extraConfig = ''
+      memory_limit = 512M
+      pdo_mysql.default_socket=''${MYSQL_UNIX_PORT}
+      mysqli.default_socket=''${MYSQL_UNIX_PORT}
+      session.save_handler = redis
+      session.save_path = "tcp://127.0.0.1:6379/0"
+    '';
+  };
+in
+{
+  languages.javascript.enable = true;
+  languages.javascript.package = pkgs.nodejs-16_x;
+
+  languages.php.enable = true;
+  languages.php.package = phpPackage;
+  languages.php.fpm.pools.web = {
+    settings = {
+      "pm" = "dynamic";
+      "pm.max_children" = 5;
+      "pm.start_servers" = 2;
+      "pm.min_spare_servers" = 1;
+      "pm.max_spare_servers" = 5;
+      "clear_env" = false;
+    };
+  };
+
+  caddy.enable = true;
+  caddy.virtualHosts."http://localhost:8000" = {
+    extraConfig = ''
+      root * /workspace/empty/public
+      php_fastcgi unix/${config.languages.php.fpm.pools.web.socket}
+      encode gzip
+      file_server
+    '';
+  };
+
+  caddy.virtualHosts."http://adminer.localhost:8000" = {
+    extraConfig = ''
+      root * ${pkgs.adminer}
+      php_fastcgi unix/${config.languages.php.fpm.pools.web.socket} {
+        index adminer.php
+      }
+      encode gzip
+      file_server
+    '';
+  };
+
+  mysql.enable = true;
+  mysql.initialDatabases = [ { name = "shopware"; }];
+
+  redis.enable = true;
+
+  env.APP_URL = "http://localhost:8000";
+  env.APP_SECRET = "devsecret";
+  env.APP_ENV = "dev";
+
+  env.DATABASE_URL = "mysql://root@localhost:3306/shopware";
+}
